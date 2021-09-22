834bd0ab5827a1095e0c434606b13e1dc70f1dad
Jim <71361453+jimcv@users.noreply.github.com>
2021-09-22 01:31:14 -0400

    Create domain_model.ump

---
+++ src/ca/mcgill/ecse223/climbsafe/model/ClimbSafe.java
@@ -0,0 +1,28 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
+
+package ca.mcgill.ecse223.climbsafe.model;
+
+// line 8 "../../../../../domain_model.ump"
+public class ClimbSafe
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public ClimbSafe()
+  {}
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {}
+
+}
