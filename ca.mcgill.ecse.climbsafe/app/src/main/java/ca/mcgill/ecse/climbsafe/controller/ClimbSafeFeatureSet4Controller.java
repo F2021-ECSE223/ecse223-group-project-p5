f3e1c14455bad3d0aa82346a09bfc71081fd73c1
Annie Kang <yiwen.kang@mail.mcgill.ca>
2021-10-25 18:15:54 -0400

    Update invalid information #3
    Repair commit

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -10,6 +10,8 @@ import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
 
 /**
  * 
+ * Controller for add equipment and update equipment
+ * 
  * @author SiboHuang
  *
  */
@@ -17,23 +19,21 @@ public class ClimbSafeFeatureSet4Controller {
 
   public static void addEquipment(String name, int weight, int pricePerWeek) throws InvalidInputException {
      
-  if (weight ==  0) {
+  if (weight <= 0) {
 	  throw new InvalidInputException("The weight must be greater than 0.");
-  } else if (weight < 0) {
-	  throw new InvalidInputException("The weight must be greater than 0");
-  } else if (pricePerWeek <= 0) {
-	  throw new InvalidInputException("The price per week must be greater that or equal to 0");
+ // } else if (weight < 0) {
+	 // throw new InvalidInputException("The weight must be greater than 0");
+  } else if (pricePerWeek < 0) {
+	  throw new InvalidInputException("The price per week must be greater than or equal to 0");
   } else if (name == null) {
 	  throw new InvalidInputException("The name must not be empty");
   } else if (name.contains("bundle")){	 
 	  throw new InvalidInputException("The equipment bundle already exists");	  
   } else {  
 	  ClimbSafe climbsafe = ClimbSafeApplication.getClimbSafe();
-  
-	  try {
 		  Equipment equipment =  new Equipment(name, weight, pricePerWeek, climbsafe);
-  }
-	  catch (RuntimeException e) {
+		  Boolean check = climbsafe.addEquipment(equipment);
+		  if (!check) {
 		  throw new InvalidInputException("The piece of "+name+" already exists");
 	  }	  
   }
@@ -43,8 +43,27 @@ public class ClimbSafeFeatureSet4Controller {
   public static void updateEquipment(String oldName, String newName, int newWeight,
       int newPricePerWeek) throws InvalidInputException {
 	  
+	  ClimbSafe climbsafe = ClimbSafeApplication.getClimbSafe();
 	  
-	  
+
+	if (newWeight == 0) {
+		throw new InvalidInputException("The weight must be greater than 0");
+	} else if (newWeight == 0) {
+		throw new InvalidInputException("The weight must be greater than 0");
+	} else if (newPricePerWeek <= 0) {
+		throw new InvalidInputException("The price per week must be greater than or equal to 0");
+	} else if (newName == null) {
+		throw new InvalidInputException("The name must not be empty");
+	} else {
+	Equipment equipment = new Equipment(newName, newWeight, newPricePerWeek, climbsafe);
+	Boolean check = climbsafe.addEquipment(equipment);
+	if (!check) {
+		throw new InvalidInputException("The piece of "+newName+" already exists");
+	}
+	
+	
+	
   }
 
+ }
 }
