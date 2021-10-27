0cdb9715c5e0ccb1715ddcf7f4050167c752ed13
Yida Pan <68798199+frankie453@users.noreply.github.com>
2021-10-27 14:05:45 -0400

    Change the FeatureTestRunner

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
@@ -6,16 +6,17 @@ import ca.mcgill.ecse.climbsafe.model.Guide;
 import ca.mcgill.ecse.climbsafe.model.User;
 
 /**
- * the controller to register guide and updateguide
+ * controller to register guide and updateguide
  * 
- * @author frankpan
+ * @author Yida Pan
  *
  */
 public class ClimbSafeFeatureSet3Controller {
+  //// reference to ClimbSafe
   private static ClimbSafe climbsafe = ClimbSafeApplication.getClimbSafe();
 
   /**
-   * the method to register guide
+   * the method to register guide and check invalid input
    * 
    * @author Yida Pan
    * @param email
@@ -25,15 +26,16 @@ public class ClimbSafeFeatureSet3Controller {
    * @throws InvalidInputException
    * 
    */
-  
+
 
   public static void registerGuide(String email, String password, String name,
+      // check invalid input and trow exceptions
       String emergencyContact) throws InvalidInputException {
-   
-         checkemail(email,name);
-         checkpassword(password);
-         checkemergencyContact(emergencyContact);
-         checkname(name);
+    checkemail(email, name);
+    checkpassword(password);
+    checkemergencyContact(emergencyContact);
+    checkname(name);
+    // add the guide to the climsafe
     try {
       climbsafe.addGuide(email, password, name, emergencyContact);
     } catch (RuntimeException e) {
@@ -42,7 +44,7 @@ public class ClimbSafeFeatureSet3Controller {
   }
 
   /**
-   * the method to update guide
+   * the method to update guide and check invalid input
    * 
    * @param email
    * @param newPassword
@@ -52,7 +54,7 @@ public class ClimbSafeFeatureSet3Controller {
    */
   public static void updateGuide(String email, String newPassword, String newName,
       String newEmergencyContact) throws InvalidInputException {
-   // checkemail(email,newName);
+    // check invalid input and throw exceptions
     checkpassword(newPassword);
     checkemergencyContact(newEmergencyContact);
     checkname(newName);
@@ -63,7 +65,28 @@ public class ClimbSafeFeatureSet3Controller {
       guide.setEmergencyContact(newEmergencyContact);
     }
   }
-  
+
+
+  /**
+   * The helper method to check if the email is in corret format
+   * 
+   * @param email
+   * @param name
+   * @throws InvalidInputException
+   */
+  private static void checkemail(String email, String name) throws InvalidInputException {
+    if (email.equals("")) {
+      throw new InvalidInputException("Email cannot be empty");
+    } else if (email.equals("admin@nmc.nt")) {
+      throw new InvalidInputException("Email cannot be admin@nmc.nt");
+    } else if (email.contains(" ")) {
+      throw new InvalidInputException("Email must not contain any spaces");
+    } else {
+
+      checklinkeduser(email);
+      checkvalidemail(email, name);
+    }
+  }
 
   /**
    * the helper method to find the guide with its email as input
@@ -71,25 +94,8 @@ public class ClimbSafeFeatureSet3Controller {
    * @author Yida Pan
    * @param email
    * @return
-   * @throws InvalidInputException 
+   * @throws InvalidInputException
    */
-  private static  void checkemail(String email,String name) throws InvalidInputException  {
-    if(email.equals("")) {
-      throw new InvalidInputException("Email cannot be empty");
-     }
-      else if(email.equals("admin@nmc.nt")) {
-        throw new InvalidInputException("Email cannot be admin@nmc.nt");
-      }
-        else if(email.contains(" ")) {
-          throw new InvalidInputException("Email must not contain any spaces");
-        }
-        else {
-          
-          checklinkeduser(email);
-          checkvalidemail(email,name);
-        }
-  }
-  
   private static Guide findGuide(String email) {
     Guide foundGuide = null;
     for (var guide : climbsafe.getGuides()) {
@@ -100,66 +106,103 @@ public class ClimbSafeFeatureSet3Controller {
     }
     return foundGuide;
   }
+
+  /**
+   * The helper method to check if the name is exmpty
+   * 
+   * @param name
+   * @throws InvalidInputException if the name is empty
+   */
   private static void checkname(String name) throws InvalidInputException {
-    if((name.equals(""))||(name==null)){
+    if ((name.equals("")) || (name == null)) {
       throw new InvalidInputException("Name cannot be empty");
     }
   }
+
+  /**
+   * The helper method to check if the password is empty
+   * 
+   * @param password
+   * @throws InvalidInputException if the password is empty
+   */
+
   private static void checkpassword(String password) throws InvalidInputException {
-    if((password.equals(""))||(password==null)){
+    if ((password.equals("")) || (password == null)) {
       throw new InvalidInputException("Password cannot be empty");
+    }
   }
-}
+
+  /**
+   * The helper method to check if the emergencycontact is empty
+   * 
+   * @param emergencyContact
+   * @throws InvalidInputException if the emergencycontact is empty
+   */
   private static void checkemergencyContact(String emergencyContact) throws InvalidInputException {
-    if((emergencyContact.equals(""))||(emergencyContact==null)){
+    if ((emergencyContact.equals("")) || (emergencyContact == null)) {
       throw new InvalidInputException("Emergency contact cannot be empty");
+    }
   }
-}
+
+  /**
+   * The helper method to check if the email is linked to existing guide or member
+   * 
+   * @param email
+   * @throws InvalidInputException if the email is linked to existing guide or member
+   */
+
   private static void checklinkeduser(String email) throws InvalidInputException {
-    var user=User.getWithEmail(email);
-    if(user!=null) {
-      if(user.getClass().equals(Guide.class)) {
+    var user = User.getWithEmail(email);
+    if (user != null) {
+      if (user.getClass().equals(Guide.class)) {
         throw new InvalidInputException("Email already linked to a guide account");
-      }
-        else {
-        throw new InvalidInputException("Email already linked to a member account");    
+      } else {
+        throw new InvalidInputException("Email already linked to a member account");
       }
     }
   }
-  private static void checkvalidemail(String email,String name) throws InvalidInputException {
-    int firstindex=email.indexOf("@");
-    StringBuilder n=new StringBuilder();
-    for(int i=firstindex;i<=firstindex+9;i++) {
-      try{n.append(email.charAt(i));
-    }
-      catch(RuntimeException e ){
+
+  /**
+   * The helper method to check if the email is in correct format
+   * 
+   * @param email
+   * @param name
+   * @throws InvalidInputException
+   */
+  private static void checkvalidemail(String email, String name) throws InvalidInputException {
+    // check if email has "@email.com" and throw exception if it does not have one
+    int firstindex = email.indexOf("@");
+    StringBuilder n = new StringBuilder();
+    for (int i = firstindex; i <= firstindex + 9; i++) {
+      try {
+        n.append(email.charAt(i));
+        // check if there is index out of bound exception
+      } catch (RuntimeException e) {
         throw new InvalidInputException("Invalid email");
       }
     }
-    String EMAIL=n.toString();
+    // throw exception
+    String EMAIL = n.toString();
     System.out.println(EMAIL);
-    if(!EMAIL.equals("@email.com")) {
-    throw new InvalidInputException("Invalid email");
+    if (!EMAIL.equals("@email.com")) {
+      throw new InvalidInputException("Invalid email");
     }
-      
-    int namelength=name.length();
-    StringBuilder m=new StringBuilder();
-    for(int j=0;j<=namelength-1;j++) {
-      try{m.append(email.charAt(j));
-    }
-      catch(RuntimeException e){
+    // check if the email has proper name in the start
+    int namelength = name.length();
+    StringBuilder m = new StringBuilder();
+    for (int j = 0; j <= namelength - 1; j++) {
+      try {
+        m.append(email.charAt(j));
+      } catch (RuntimeException e) {
         throw new InvalidInputException("Invalid email");
-      } 
+      }
     }
-    String NAME=m.toString();
+    String NAME = m.toString();
     if (!NAME.equalsIgnoreCase(name)) {
       throw new InvalidInputException("Invalid email");
-    
-  }
-  }
-  }
-  
 
-
+    }
+  }
+}
 
 
--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/CucumberFeaturesTestRunner.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/CucumberFeaturesTestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/resources/RegisterGuide.feature")
+@CucumberOptions(plugin = "pretty", features = "src/test/resources")
 public class CucumberFeaturesTestRunner {
 }
