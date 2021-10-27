078812fa1b78616947b414b1dde93a6035d80b38
oliverqueen1009 <sibo.huang@mail.mcgill.ca>
2021-10-27 16:44:06 -0400

    Formatting for Set4Controller

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -14,94 +14,113 @@ import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
  *
  */
 public class ClimbSafeFeatureSet4Controller {
-  
+
   // reference to ClimbSafe
   private static ClimbSafe climbsafe = ClimbSafeApplication.getClimbSafe();
-  public static void addEquipment(String name, int weight, int pricePerWeek) throws InvalidInputException {
-  
-   /*
-    * Validate weight & pricePerWeek & name
-    */
-   
-  if (weight <= 0) {
-	  throw new InvalidInputException("The weight must be greater than 0");
-  } else if (pricePerWeek < 0) {
-	  throw new InvalidInputException("The price per week must be greater than or equal to 0");
-  } else if (name == null || name.equals("") || name.equals(" ")) {
-    throw new InvalidInputException("The name must not be empty"); 
-  }
-  
-  /*
-   * Validate existent of equipment and equipment bundle in the system
+
+  /**
+   * 
+   * @param name
+   * @param weight
+   * @param pricePerWeek
+   * @throws InvalidInputException
    */
   
-  var equip = BookableItem.getWithName(name);
+  public static void addEquipment(String name, int weight, int pricePerWeek)
+      throws InvalidInputException {
+
+    /*
+     * Validate weight & pricePerWeek & name
+     */
+
+    if (weight <= 0) {
+      throw new InvalidInputException("The weight must be greater than 0");
+    } else if (pricePerWeek < 0) {
+      throw new InvalidInputException("The price per week must be greater than or equal to 0");
+    } else if (name == null || name.equals("") || name.equals(" ")) {
+      throw new InvalidInputException("The name must not be empty");
+    }
+
+    /*
+     * Validate existent of equipment and equipment bundle in the system
+     */
+
+    var equip = BookableItem.getWithName(name);
     if (equip instanceof Equipment) {
       throw new InvalidInputException("The piece of equipment already exists");
-    }   
-  
-  
-  var equipbun = BookableItem.getWithName(name);
-  if (equipbun != null) {
-    if (equipbun instanceof EquipmentBundle) {
-      throw new InvalidInputException("The equipment bundle already exists");
     }
-  }
-  
-  climbsafe.addEquipment(name, weight, pricePerWeek);
-}
 
+
+    var equipbun = BookableItem.getWithName(name);
+    if (equipbun != null) {
+      if (equipbun instanceof EquipmentBundle) {
+        throw new InvalidInputException("The equipment bundle already exists");
+      }
+    }
+
+    climbsafe.addEquipment(name, weight, pricePerWeek);
+  }
+
+  
+  /**
+   * 
+   * @param oldName
+   * @param newName
+   * @param newWeight
+   * @param newPricePerWeek
+   * @throws InvalidInputException
+   */
 
   public static void updateEquipment(String oldName, String newName, int newWeight,
       int newPricePerWeek) throws InvalidInputException {
-	
+
     /*
      * Validate newWeight & newPricePerWeek & newName
      */
 
-	if (newWeight <= 0) {
-		throw new InvalidInputException("The weight must be greater than 0");
-	} else if (newPricePerWeek <= 0) {
-		throw new InvalidInputException("The price per week must be greater than or equal to 0");
-	} else if (newName.equals("")) {
-		throw new InvalidInputException("The name must not be empty");
-	} else if (newName.equals(" ")) {
-	    throw new InvalidInputException("The name must not be empty");
-	}
-	
-	/*
-	 * Validate existent of equipment and equipment bundle in the system
-	 */
-	
-	var equip = BookableItem.getWithName(oldName);
-	if (equip == null) {
-	  throw new InvalidInputException("The piece of equipment does not exist");
-	}  
-	
-	var newequip = BookableItem.getWithName(newName);
-	if (!newName.equals(oldName)) {
-	  if (newequip instanceof Equipment) {
-	      throw new InvalidInputException("The piece of equipment already exists");
-	    }
-	    
-	    if (newequip instanceof EquipmentBundle) {
-	      throw new InvalidInputException("An equipment bundle with the same name already exists");
-	    }
-	  
-	}
-	
-	// update the equipment with new information
+    if (newWeight <= 0) {
+      throw new InvalidInputException("The weight must be greater than 0");
+    } else if (newPricePerWeek <= 0) {
+      throw new InvalidInputException("The price per week must be greater than or equal to 0");
+    } else if (newName.equals("")) {
+      throw new InvalidInputException("The name must not be empty");
+    } else if (newName.equals(" ")) {
+      throw new InvalidInputException("The name must not be empty");
+    }
 
-	var checker = BookableItem.getWithName(oldName);
-	
-	if (checker != null && checker instanceof Equipment) {
-	  var found_equip = (Equipment) checker;
-	  
-	  found_equip.setName(newName);
-	  found_equip.setWeight(newWeight);
-	  found_equip.setPricePerWeek(newPricePerWeek);
-	}
-	
-	} 
+    /*
+     * Validate existent of equipment and equipment bundle in the system
+     */
+
+    var equip = BookableItem.getWithName(oldName);
+    if (equip == null) {
+      throw new InvalidInputException("The piece of equipment does not exist");
+    }
+
+    var newequip = BookableItem.getWithName(newName);
+    if (!newName.equals(oldName)) {
+      if (newequip instanceof Equipment) {
+        throw new InvalidInputException("The piece of equipment already exists");
+      }
+
+      if (newequip instanceof EquipmentBundle) {
+        throw new InvalidInputException("An equipment bundle with the same name already exists");
+      }
+
+    }
+
+    // update the equipment with new information
+
+    var checker = BookableItem.getWithName(oldName);
+
+    if (checker != null && checker instanceof Equipment) {
+      var found_equip = (Equipment) checker;
+
+      found_equip.setName(newName);
+      found_equip.setWeight(newWeight);
+      found_equip.setPricePerWeek(newPricePerWeek);
+    }
+
+  }
 
 }
