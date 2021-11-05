da846e7123883dd777972450f5d381132dd621de
Annie Kang <yiwen.kang@mail.mcgill.ca>
2021-11-05 13:43:28 -0400

    add methods

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,5 +1,108 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
-public class AssignmentController {
 
+import ca.mcgill.ecse.climbsafe.model.Member;
+import ca.mcgill.ecse.climbsafe.model.User;
+
+public class AssignmentController {
+  
+ 
+/**
+ * 
+ * @param email
+ * @throws InvalidInputException
+ */
+  public static void cancelTrip(String email) throws InvalidInputException {
+    
+    validate_member(email);
+    var user= User.getWithEmail(email);
+    var member  = (Member) user;
+  
+  throw new InvalidInputException("");
+  
+  }
+/**
+ * 
+ * @param email
+ * @throws InvalidInputException
+ */
+  public static void finishTrip(String email) throws InvalidInputException {
+    
+  throw new InvalidInputException("");
+  
+  }
+  
+ /**
+  * 
+  * @param week
+  * @throws InvalidInputException
+  */
+  
+  public static void startTripsForWeek(int week) throws InvalidInputException {
+    
+  throw new InvalidInputException("");
+  
+  }
+  
+ /**
+  * 
+  * @param email
+  * @param code
+  * @throws InvalidInputException
+  */
+  
+  public static void confirmPayment(String email, String code) throws InvalidInputException {
+    
+    validate_member(email);
+    var user= User.getWithEmail(email);
+    var member  = (Member) user;
+    
+    //update state
+    member.getAssignment().doAssign();
+       
+   //------------------------------------------------------------------------------------------- 
+    
+    if (code == null || code.equals("") || code.equals(" ")) {
+      throw new InvalidInputException("Invalid authorization code");
+    }
+   
+    //update state
+    member.Pay(code);
+  
+  }
+  /**
+   * 
+   * @param email
+   * @throws InvalidInputException
+   */
+  public static void toggleBan(String email) throws InvalidInputException {
+    
+    validate_member(email);
+    var user= User.getWithEmail(email);
+    var member  = (Member) user;
+    
+  //update state
+    member.toggleBan();
+   
+  }
+  /**
+   * Helper method for validating input email
+   * 
+   * @param discount
+   * @throws InvalidInputException
+   */
+  private static void validate_member(String email) throws InvalidInputException {
+   
+    var user= User.getWithEmail(email);
+    if (user == null) {
+      throw new InvalidInputException("Member with email address "+email+" does not exist");
+    } else if (!(user instanceof Member)) {
+      throw new InvalidInputException("Member with email address "+email+" does not exist");
+    }
+   
+  }
+
+  
+  
+  
 }
