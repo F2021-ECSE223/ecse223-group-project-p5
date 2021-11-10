96412fbb6c3966781409b7d0484d4634028ebbfc
Annie Kang <yiwen.kang@mail.mcgill.ca>
2021-11-10 11:02:39 -0500

    update assignment controller

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,6 +1,13 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.Assignment;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.Guide;
+import ca.mcgill.ecse.climbsafe.model.Hotel;
 import ca.mcgill.ecse.climbsafe.model.Member;
 import ca.mcgill.ecse.climbsafe.model.User;
 
@@ -17,8 +24,21 @@ public class AssignmentController {
     validate_member(email);
     var user= User.getWithEmail(email);
     var member  = (Member) user;
-  
-  throw new InvalidInputException("");
+
+    switch(member.getBanStatus()) {
+      case Banned:
+        throw new InvalidInputException("Cannot cancel the trip due to a ban");
+      default:
+        break;
+    }
+    switch(member.getAssignment().getAssignmentStatus()) {
+      case Finished:
+        throw new InvalidInputException("Cannot cancel a trip which has finished");
+      default:
+        break;
+    }
+    //cancel assignment
+    member.getAssignment().cancel();
   
   }
 /**
@@ -27,9 +47,34 @@ public class AssignmentController {
  * @throws InvalidInputException
  */
   public static void finishTrip(String email) throws InvalidInputException {
+   
+    validate_member(email);
+    var user= User.getWithEmail(email);
+    var member  = (Member) user;
     
-  throw new InvalidInputException("");
-  
+    switch(member.getBanStatus()) {
+      case Banned:
+        throw new InvalidInputException("Cannot finish the trip due to a ban");
+      default:
+        break;
+    }
+    
+    switch(member.getAssignment().getAssignmentStatus()) {
+      case Assigned:
+        throw new InvalidInputException("Cannot finish a trip which has not started");
+       
+      case Paid:
+        throw new InvalidInputException("Cannot finish a trip which has not started");
+      case Cancelled:
+        throw new InvalidInputException("Cannot finish a trip which has been cancelled");
+      default:
+        break;
+              
+    }
+    
+    //at valid state-> do transition
+    member.getAssignment().finish();
+      
   }
   
  /**
@@ -40,8 +85,32 @@ public class AssignmentController {
   
   public static void startTripsForWeek(int week) throws InvalidInputException {
     
-  throw new InvalidInputException("");
-  
+   // reference to ClimbSafe
+    ClimbSafe cs = ClimbSafeApplication.getClimbSafe();
+    
+    for (Assignment a : cs.getAssignments()) {
+      if (a.getStartWeek() == 4) {
+        switch(a.getMember().getBanStatus()) {
+          case Banned:
+            throw new InvalidInputException("Cannot start the trip due to a ban");
+          default:
+            break;
+        }
+        switch(a.getAssignmentStatus()) {
+          case Cancelled:
+            throw new InvalidInputException("Cannot start a trip which has been cancelled");
+          case Finished:
+            throw new InvalidInputException("Cannot start a trip which has finished");
+          default:
+            break;
+        }
+        //valid status
+        a.start();
+         
+      
+      }
+    }
+ 
   }
   
  /**
@@ -58,16 +127,44 @@ public class AssignmentController {
     var member  = (Member) user;
     
     //update state
-    member.getAssignment().doAssign();
-       
-   //------------------------------------------------------------------------------------------- 
+    int startWeek = member.getAssignment().getStartWeek();
+    int endWeek = member.getAssignment().getEndWeek();
+    Guide guide = member.getAssignment().getGuide();
+    Hotel hotel = member.getAssignment().getHotel();
     
+    //assign assignment
+    member.getAssignment().assign(startWeek, endWeek, guide, hotel);
+   
     if (code == null || code.equals("") || code.equals(" ")) {
       throw new InvalidInputException("Invalid authorization code");
     }
+    
+    switch(member.getBanStatus()) {
+      case Banned:
+        throw new InvalidInputException("Cannot pay for the trip due to a ban");
+      default:
+        break;
+    }
    
+    switch(member.getAssignment().getAssignmentStatus()) {
+      case Started:
+        throw new InvalidInputException("Trip has already been paid for");
+       
+      case Paid:
+        throw new InvalidInputException("Trip has already been paid for");
+      
+      case Cancelled:
+        throw new InvalidInputException("Cannot pay for a trip which has been cancelled");
+      
+      case Finished:
+        throw new InvalidInputException("Cannot pay for a trip which has finished");
+      default:
+        break;
+              
+    }
+    
     //update state
-    member.Pay(code);
+    member.getAssignment().pay(code);
   
   }
   /**
