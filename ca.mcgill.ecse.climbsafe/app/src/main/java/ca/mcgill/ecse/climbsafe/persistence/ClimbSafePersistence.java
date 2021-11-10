e9b631ef1e784cd4c27004ffff3497f381b8c094
Jim <71361453+jimcv@users.noreply.github.com>
2021-11-10 17:42:21 -0500

    Persistence Package Done

---
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/persistence/ClimbSafePersistence.java
@@ -0,0 +1,37 @@
+package ca.mcgill.ecse.climbsafe.persistence;
+
+import java.sql.Date;
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+
+public class ClimbSafePersistence {
+
+  private static String filename = "data.btms";
+
+  public static void setFilename(String filename) {
+    ClimbSafePersistence.filename = filename;
+  }
+
+  public static void save() {
+    PersistenceObjectStream.setFilename(filename);
+    save(ClimbSafeApplication.getClimbSafe());
+  }
+
+  public static void save(ClimbSafe climbsafe) {
+    PersistenceObjectStream.setFilename(filename);
+    PersistenceObjectStream.serialize(climbsafe);
+  }
+
+  public static ClimbSafe load() {
+    PersistenceObjectStream.setFilename(filename);
+    var climbsafe = (ClimbSafe) PersistenceObjectStream.deserialize();
+    // model cannot be loaded - create empty BTMS
+    if (climbsafe == null) {
+      climbsafe = new ClimbSafe(new Date(0), 0, 0);
+    } else {
+      climbsafe.reinitialize();
+    }
+    return climbsafe;
+  }
+
+}
