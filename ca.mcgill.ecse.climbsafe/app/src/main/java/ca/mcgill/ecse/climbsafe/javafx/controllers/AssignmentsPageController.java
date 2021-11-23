4996e77bf6078f313f9c6632e146839561690791
michaelg29 <michaelgriecocoding@gmail.com>
2021-11-23 01:34:24 -0500

    Redesigned assignments page

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/controllers/AssignmentsPageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/controllers/AssignmentsPageController.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse.climbsafe.javafx.controllers;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import ca.mcgill.ecse.climbsafe.controller.AssignmentController;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet6Controller;
@@ -25,41 +26,42 @@ public class AssignmentsPageController {
 	private Button manageTripButton;
 	@FXML
 	private TableView<TOAssignment> overviewTable;
+	@FXML
+	private Label selectedMemberLabel;
+	@FXML
+    private Label selectedGuideLabel;
+	@FXML
+    private Label selectedHotelLabel;
+	@FXML
+    private Label selectedDurationLabel;
+	@FXML
+    private Label selectedGuideCostLabel;
+	@FXML
+    private Label selectedEquipmentCostLabel;
+	@FXML
+    private Label selectedPaymentCodeLabel;
+	@FXML
+    private Label selectedStatusLabel;
 	
 	@FXML
 	public void initialize() {
-	  overviewTable.getColumns().add(createTableColumn("Member Email", "memberEmail"));
-	  overviewTable.getColumns().add(createTableColumn("Member Name", "memberName"));
-	  overviewTable.getColumns().add(createTableColumn("Guide Email", "guideEmail"));
-	  overviewTable.getColumns().add(createTableColumn("Guide Name", "guideName"));
-	  overviewTable.getColumns().add(createTableColumn("Hotel Name", "hotelName"));
-	  overviewTable.getColumns().add(createTableColumn("Start Week", "startWeek"));
-	  overviewTable.getColumns().add(createTableColumn("End Week", "endWeek"));
-	  overviewTable.getColumns().add(createTableColumn("Guide Cost", "totalCostForGuide"));
-	  overviewTable.getColumns().add(createTableColumn("Equipment Cost", "totalCostForEquipment"));
-	  overviewTable.getColumns().add(createTableColumn("Status", "status"));
-	  overviewTable.getColumns().add(createTableColumn("Authorization Code", "authorizationCode"));
-	
-	  // only show refund if cancelled or finished
-	  var refundColumn = new TableColumn<TOAssignment, String>("Refund");
-	  refundColumn.setCellValueFactory(data -> Bindings.createStringBinding(
-	      () -> {
-	        String status = data.getValue().getStatus();
-	        if (status.equals("Finished") || status.equals("Cancelled")) {
-	          return String.valueOf(data.getValue().getRefundedPercentageAmount());
-	        }
-	        else {
-	          return null;
-	        }
-	      }));
-	  overviewTable.getColumns().add(refundColumn);
+	  // assign member name and email to columns of overview table
+	  overviewTable.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("memberName"));
+	  overviewTable.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("memberEmail"));
 	  
 	  // double click listener on overview table
 	  overviewTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
 	    @Override
 	    public void handle(MouseEvent mouseEvent) {
 	      if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
-	        if (mouseEvent.getClickCount() == 2) {
+	        if (mouseEvent.getClickCount() == 1) {
+	          // display information for selected assignment
+	          TOAssignment target = overviewTable.getSelectionModel().getSelectedItem();
+	          if (target != null) {
+	            updateDetailsWindow(target);
+	          }
+	        }
+	        else if (mouseEvent.getClickCount() == 2) {
 	          manageTripPressed(null);
 	        }
 	      }
@@ -85,6 +87,49 @@ public class AssignmentsPageController {
 	  TOAssignment target = overviewTable.getSelectionModel().getSelectedItem();
 	  if (target != null) {
 	    ClimbSafeView.getInstance().activateTripsPage(target.getMemberEmail());
+	  }
+	}
+	
+	private void updateDetailsWindow(TOAssignment target) {
+	  selectedMemberLabel.setText(String.format("%s (%s)", target.getMemberName(), target.getMemberEmail()));
+	  
+	  if (target.getGuideEmail() != null && !target.getGuideEmail().isEmpty()) {
+	    selectedGuideLabel.setText(String.format("%s (%s)", target.getGuideName(), target.getGuideEmail()));
+	  }
+	  else {
+	    selectedGuideLabel.setText("Not assigned");
+	  }
+	  
+	  if (target.getHotelName() != null && !target.getHotelName().isEmpty()) {
+        selectedHotelLabel.setText(String.format("%s", target.getHotelName()));
+      }
+      else {
+        selectedHotelLabel.setText("Not assigned");
+      }
+	  
+	  selectedDurationLabel.setText(String.format("Week %d -> Week %d", target.getStartWeek(), target.getEndWeek()));
+	  
+	  selectedGuideCostLabel.setText(String.format("$%dNEP", target.getTotalCostForGuide()));
+	  
+	  selectedEquipmentCostLabel.setText(String.format("$%dNEP", target.getTotalCostForEquipment()));
+	  
+	  if (target.getAuthorizationCode() != null && !target.getAuthorizationCode().isEmpty()) {
+	    selectedPaymentCodeLabel.setText(String.format("%s", target.getAuthorizationCode()));
+      }
+      else {
+        selectedPaymentCodeLabel.setText("Not paid yet");
+      }
+	  
+	  switch (target.getStatus()) {
+	    case "Unassigned":
+	    case "Assigned":
+	    case "Paid":
+	    case "Started":
+	      selectedStatusLabel.setText(target.getStatus());
+	      break;
+	    case "Finished":
+	    case "Cancelled":
+	      selectedStatusLabel.setText(String.format("%s (%d%% refund)", target.getStatus(), target.getRefundedPercentageAmount()));
 	  }
 	}
 	
